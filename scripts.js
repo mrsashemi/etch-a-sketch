b4f2bc2581ab6677c5526e71cba6dc927a9469e6
mrsashemi <mrsashemi@gmail.com>
2022-02-12 17:31:36 -0800

    Clean code and fix typos in text

--- scripts.js
+++ scripts.js
@@ -8,9 +8,6 @@ const gridValue = document.querySelector('.gridvalue');
 const lightValue = document.querySelector('.lightvalue');
 const output = document.querySelector("#value");
 const rangeColor = document.querySelector('#colorrange');
-const portalConcept = document.querySelector('.portalconcept')
-const portalExplain = document.querySelector('.portalexplanation');
-const project = document.querySelector('.project');
 const lightExplain = document.querySelector('.changelightexplain');
 const sizeOutput = document.querySelector('#values');
 const sizeSlider = document.querySelector('#sizerange');
@@ -20,6 +17,8 @@ const sizeValue = document.querySelector('.sizevalue');
 const sliderArr = [4, 8, 12, 16, 32];
 let x = 0;
 
+
+// The following functions create and style the grid
 function createPortalGrid(n) {
     let squares = (Math.pow(n, 2))/2;
     for (let i = 0; i < squares; i++) {
@@ -42,6 +41,7 @@ createPortalGrid(sliderArr[x]);
 
 function stylePortalGrid() {
     let sparkle = document.querySelectorAll(".sparkle");
+    // Use for each method to iterate through the node array
     sparkle.forEach(el => {
         el.addEventListener('mouseover', (e) => {
             let randomColor = Math.floor(Math.random()*16777215).toString(16);
@@ -94,7 +94,6 @@ function stylePortalGlowGrid() {
     });
 }
 
-output.textContent = `${sliderArr[x]} x ${(sliderArr[x])/2}`;
 
 portal.style.width = '400px';
 portal.style.height = '800px';
@@ -107,8 +106,9 @@ glowContainer.style.width = '400px';
 glowContainer.style.height = '800px';
 
 sizeOutput.textContent = 
-`Use this slider to adjust the height and width of portal to your preferred screen size`;
+`Height: 800. Use this slider to adjust the height and width of portal to your preferred screen size`;
 
+// Adjust size of the etch-a-sketch to accomodate different screen sizes
 function changeSize() {
     sizeSlider.addEventListener('input', () => {
         portal.style.width = `${sizeSlider.value/2}px`;
@@ -120,11 +120,13 @@ function changeSize() {
         portalContainer.style.height = `${sizeSlider.value}px`;
         glowContainer.style.width = `${sizeSlider.value/2}px`;
         glowContainer.style.height = `${sizeSlider.value}px`;
-        sizeOutput.textContent = `Height: ${sizeSlider.value}. Please adjust to your screen size`;
+        sizeOutput.textContent = `Height: ${sizeSlider.value}. Use this slider to adjust the height and width of portal to your preferred screen size`;
     })
 }
 
 changeSize();
+
+output.textContent = `${sliderArr[x]} x ${(sliderArr[x])/2}`;
 
 function changeGrid() {
     gridSlider.addEventListener('input', () => {
@@ -150,33 +152,7 @@ gridSlider.addEventListener('input', () => {
     reset();
 });
 
-portalConcept.textContent =
-`Portal is an ineractive, glowing work of art created by Hasib Hashemi 
- and Adeeb Djaward of WizardsRobbingKids. The piece itself was created
- using epoxy resin and is actually two different images with a two way
- mirror in-between. Depending on the time of day, a different image is
- shown.`
-
-project.textContent = 
-`Using scans of the original art piece, I adapted this Javascript 
- etch-a-sketch project into a demonstration of the piece itself. 
- Use the above slider to adjust the grid size or to reset.`
-
-portalExplain.textContent =
- `This slider represents the lighting and adjust the opacity of the image 
- to reveal the image below. You'll notice the sketch effect changes as 
- the opacity is adjusted. Under 50% and the etch-a-sketch will change completely.
- Depending on the light, you beginlmao to see both images simultaneously.`
-
-lightExplain.textContent = 
- `During the day, portal is a mirror with a 3D pattern transparently
- oil painted over the top of it. The primary colors are glazed over 
- layers of clear epoxy resin. Holographic film is used in place of 
- paint over one side of the pattern. As light travels through top 
- layers, it bounces off the mirror reflecting a rainbow.
- If the lighting is just right, the viewer can see themselves in the image.`
-
-
+// Slider that adjusts opacity of both etch-a-sketch and page to represent different lighting conditions
 function changeColor() {
     let colorRange = rangeColor.value;
     let color = `rgb(${colorRange}, ${colorRange}, ${colorRange})`;
@@ -203,8 +179,8 @@ function changeColor() {
         oil painted over the top of it. The primary colors are glazed over 
         layers of clear epoxy resin. Holographic film is used in place of 
         paint over one side of the pattern. As light travels through top 
-        layers, it bounces off the mirror reflecting a rainbow.
-        If the lighting is just right, the viewer can see themselves in the image.`
+        layers, it bounces off the mirror reflecting the viewer and the hologram
+        If the lighting is just right, the piece reflects a rainbow.`
     }
 }
 
